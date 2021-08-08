6f4249cd47a289999b3b76d4a778a0d920c4ebe4
Necrosis <60231561+N3cr0s1s@users.noreply.github.com>
2021-08-08 11:13:38 +0200

    v0.1

--- src/main/java/me/necrosis/configapi/ConfigAPI.java
+++ src/main/java/me/necrosis/configapi/ConfigAPI.java
@@ -4,8 +4,6 @@ import me.necrosis.configapi.config.ConfigProvider;
 import me.necrosis.configapi.reflect.AnnotationScan;
 import me.necrosis.configapi.reflect.MainPluginGetter;
 import me.necrosis.configapi.reflect.SaveProvider;
-import me.necrosis.configapi.test.TestClass;
-import me.necrosis.configapi.test.TestClass2;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConfigAPI extends JavaPlugin {
@@ -16,18 +14,10 @@ public final class ConfigAPI extends JavaPlugin {
     private final AnnotationScan annotationScan = new AnnotationScan(this);
     private final SaveProvider   saveProvider   = new SaveProvider(this);
     private final MainPluginGetter mainPluginGetter = new MainPluginGetter();
-    private TestClass testClass;
 
     @Override
     public void onEnable() {
         this.configAPI = this;
-
-        testClass = new TestClass();
-        getLogger().info( testClass.getFields());
-        testClass.mentsTeKutya();
-        getLogger().info( testClass.getFields());
-
-        new TestClass2().saveConfig();
     }
 
     @Override
@@ -36,10 +26,6 @@ public final class ConfigAPI extends JavaPlugin {
     }
 
     public static ConfigAPI getConfigAPI() {
-        return configAPI;
-    }
-
-    private static ConfigAPI getInstance(){
         return configAPI;
     }
 
